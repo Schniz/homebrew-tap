0ba50f131be44b500b9e8fcc9579c93fb94d6782
Gal Schlezinger <gal@spitfire.co.il>
2019-03-18 11:29:41 +0200

    Fnm v1.8.0

---
+++ Formula/fnm.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Fnm formula :D
+class Fnm < Formula
+  VERSION = '1.8.0'
+  desc 'Fast and simple Node.js version manager'
+  homepage 'https://github.com/Schniz/fnm'
+  url "https://github.com/Schniz/fnm/releases/download/v#{VERSION}/fnm-macos.zip"
+  version VERSION
+  sha256 'ee844e5edf0c61d0bc9f15ec8eab63e7756057e0c5c88678874e14f4ebaf7abe'
+
+  bottle :unneeded
+
+  def install
+    bin.install 'fnm'
+  end
+
+  def caveats
+    if File.read(File.expand_path(shell_profile)).include?('# fnm')
+      <<~CAVEATS
+        Skipped the installation of fnm to #{shell_profile} because I found `# fnm`.
+        In case you want to do it manually, add the following to the file:
+
+          # fnm
+          #{source_for_shell}
+      CAVEATS
+    else
+      setup_shells
+      "No `# fnm` in #{shell_profile}, so fnm was installed to it."
+    end
+  end
+
+  def source_for_shell
+    if preferred == 'fish'
+      'fnm env --multi | source'
+    else
+      %{eval "$(fnm env --multi)"}
+    end
+  end
+
+  def setup_shells
+    File.open(File.expand_path(shell_profile), 'a') do |f|
+      f << "# fnm\n"
+      f << "#{source_for_shell}\n"
+    end
+  end
+
+  test do
+    system "#{bin}/fnm", '--version'
+  end
+end
