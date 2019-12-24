0cc6144490252caa04a6a1f34404d38aed57b3b8
Gal Schlezinger <gal@spitfire.co.il>
2019-12-24 14:38:17 +0200

    Fix missing file in fnm (#2)

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -16,7 +16,7 @@ class Fnm < Formula
   end
 
   def caveats
-    if File.read(File.expand_path(shell_profile)).include?('# fnm')
+    if fnm_already_configured?
       <<~CAVEATS
         Skipped the installation of fnm to #{shell_profile} because I found `# fnm`.
         In case you want to do it manually, add the following to the file:
@@ -28,6 +28,13 @@ class Fnm < Formula
       setup_shells
       "No `# fnm` in #{shell_profile}, so fnm was installed to it."
     end
+  end
+
+  def fnm_already_configured?
+    File.read(File.expand_path(shell_profile)).include?('# fnm')
+  rescue
+    puts "Can't read shell profile!"
+    false
   end
 
   def source_for_shell
